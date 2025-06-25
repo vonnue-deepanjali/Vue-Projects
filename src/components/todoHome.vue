e780f186bad3b29644af7319111ace4ba64d3269
vonnue-deepanjali <deepanjali.dilish@vonnue.com>
2025-06-25 10:02:55 +0530

    update:media

--- src/components/todoHome.vue
+++ src/components/todoHome.vue
@@ -191,6 +191,12 @@ const deleteAllTasks = async () => {
     white-space: nowrap;
   }
 
+  @media only screen and (max-width: 600px) {
+    &-task-name {
+      max-width: 142px !important;
+    }
+  }
+
   &-tooltip-text {
     max-width: 400px;
     white-space: normal;
